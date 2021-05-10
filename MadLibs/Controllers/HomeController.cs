48f12abc17e933ea61345a7ed84028db77a60add
Usarneme <jamestcoop@gmail.com>
2021-05-10 11:25:30 -0700

    updates madlib to king arthur story using 11 elements
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -10,11 +10,20 @@ namespace MadLibs.Controllers
     public ActionResult Form() { return View(); }
 
     [Route("/MadLib")]
-    public ActionResult MadLib(string noun, string verb)
+    public ActionResult MadLib(string adjective1, string adjective2, string adjective3, string adjective4, string adverb, string articleOfClothing, string job1, string job2, string noun1, string noun2, string pluralAnimal)
     {
       MadLibCreator myMadLibVariable = new MadLibCreator();
-      myMadLibVariable.Noun = noun;
-      myMadLibVariable.Verb = verb;
+      myMadLibVariable.Adjective1 = adjective1;
+      myMadLibVariable.Adjective2 = adjective2;
+      myMadLibVariable.Adjective3 = adjective3;
+      myMadLibVariable.Adjective4 = adjective4;
+      myMadLibVariable.Adverb = adverb;
+      myMadLibVariable.ArticleOfClothing = articleOfClothing;
+      myMadLibVariable.Job1 = job1;
+      myMadLibVariable.Job2 = job2;
+      myMadLibVariable.Noun1 = noun1;
+      myMadLibVariable.Noun2 = noun2;
+      myMadLibVariable.PluralAnimal = pluralAnimal;
       return View(myMadLibVariable);
     }
 
--- MadLibs/Models/MadLibs.cs
+++ MadLibs/Models/MadLibs.cs
@@ -2,7 +2,16 @@ namespace MadLibs.Models
 {
   public class MadLibCreator
   {
-    public string Noun { get; set; }
-    public string Verb { get; set; }
+    public string Adjective1 { get; set; }
+    public string Adjective2 { get; set; }
+    public string Adjective3 { get; set; }
+    public string Adjective4 { get; set; }
+    public string Adverb { get; set; }
+    public string ArticleOfClothing { get; set; }
+    public string Job1 { get; set; }
+    public string Job2 { get; set; }
+    public string Noun1 { get; set; }
+    public string Noun2 { get; set; }
+    public string PluralAnimal { get; set; }
   }
 }
